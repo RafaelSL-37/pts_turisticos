9738353ada7f0acd61da26d9836c1812309f660b
Rafael SL <rafaelsilvadelima37@gmail.com>
2020-12-25 19:39:52 -0300

    reconfiguring

--- pts_turisticos/pts_turisticos/settings.py
+++ pts_turisticos/pts_turisticos/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pts-turisticos.herokuapp.com', 'localhost:8000']
-
+#ALLOWED_HOSTS = ['pts-turisticos.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
