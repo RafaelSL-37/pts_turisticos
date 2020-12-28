82d5d6e0eef66c0bc4b2ee7a012b6419ad33477a
Rafael SL <rafaelsilvadelima37@gmail.com>
2020-12-28 20:02:03 -0300

    moving files

--- pts_turisticos/settings.py
+++ pts_turisticos/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'rest_framework',
+    'pts_turisticos',
     'pontos_turisticos',
     'atracoes',
     'comentarios',
